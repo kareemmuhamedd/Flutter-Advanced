898158f7cbdcfca4a376c76ca7fa4b9159eeb9f4
kareemmuhamedd <kareemmuhamed2g@gmail.com>
2024-06-19 22:16:47 +0300

    - added images & svgs folders for assets - added flutter_svg & added flutter_screenutil - made a file for doc app clinet - added navigation extension - added routing folder that conatins routers class and file - added colors & text styles - added empty login screen - added onboarding screen

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,31 @@
+import 'package:doc_app/core/routing/routes.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../features/login/ui/login_screen.dart';
+import '../../features/onboarding/onboarding_screen.dart';
+
+class AppRouter {
+  Route generateRouter(RouteSettings settings) {
+
+    final arguments = settings.arguments;
+    switch (settings.name) {
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(
+          builder: (_) => const OnboardingScreen(),
+        );
+      case Routes.loginScreen:
+        return MaterialPageRoute(
+          builder: (_) => const LoginScreen(),
+        );
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(
+              child: Text('No route defined for ${settings.name}'),
+            ),
+          ),
+        );
+    }
+  }
+}
